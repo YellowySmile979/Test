2c73f93893a177aa53190951eb67f867ff44f6fe
YellowySmile979 <xcant5@gmail.com>
2022-11-24 16:08:48 +0800

    CHANGE LOG 4 (ADDED RIGIDBODY2D)

--- Test/Assets/Script/gay.cs
+++ Test/Assets/Script/gay.cs
@@ -8,11 +8,13 @@ public class gay : MonoBehaviour
     public float jumpSpeed = 7;
     public float moveSpeed = 5;
 
+    Rigidbody2D rb;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
+
     }
 
     // Update is called once per frame
